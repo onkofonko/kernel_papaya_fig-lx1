d91e8d179772dd0e75eb5157b2b5e538db6fa035
ockysaurus <ocky852@gmail.com>
2019-12-29 21:06:43 +0100

    Fix build error tcp_input

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -7102,7 +7102,7 @@ int tcp_conn_request(struct request_sock_ops *rsk_ops,
 		if (!want_cookie)
 			goto drop;
 	}
-
+#endif
 	if (sk_acceptq_is_full(sk)) {
 		NET_INC_STATS(sock_net(sk), LINUX_MIB_LISTENOVERFLOWS);
 		goto drop;
