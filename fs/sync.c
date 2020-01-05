85aee51387202db50333de432af830f47c88a2a0
ockysaurus <ocky852@gmail.com>
2020-01-05 19:14:08 +0100

    Revert "Delete Fsync switch, add  Dynamic Fsync"
    This reverts commit 6f586e39df3fa1ed206c2f3ac76282e80db7d0c6.

--- fs/sync.c
+++ fs/sync.c
@@ -7,6 +7,7 @@
 #include <linux/fs.h>
 #include <linux/slab.h>
 #include <linux/export.h>
+#include <linux/module.h>
 #include <linux/namei.h>
 #include <linux/sched.h>
 #include <linux/writeback.h>
@@ -17,9 +18,8 @@
 #include <linux/backing-dev.h>
 #include "internal.h"
 
-#ifdef CONFIG_DYNAMIC_FSYNC
-#include <linux/dyn_sync_cntrl.h>
-#endif
+bool fsync_enabled = true;
+module_param(fsync_enabled, bool, 0755);
 
 #define VALID_FLAGS (SYNC_FILE_RANGE_WAIT_BEFORE|SYNC_FILE_RANGE_WRITE| \
 			SYNC_FILE_RANGE_WAIT_AFTER)
@@ -98,30 +98,6 @@ static void fdatawait_one_bdev(struct block_device *bdev, void *arg)
 	filemap_fdatawait_keep_errors(bdev->bd_inode->i_mapping);
 }
 
-#ifdef CONFIG_DYNAMIC_FSYNC
-/*
- * Sync all the data for all the filesystems (called by sys_sync() and
- * emergency sync)
- */
-void sync_filesystems(int nowait)
-{
-	/*
-	 * Sync twice to reduce the possibility we skipped some inodes / pages
-	 * because they were temporarily locked
-	 */
-
-	iterate_supers(sync_inodes_one_sb, &nowait);
-	iterate_supers(sync_fs_one_sb, &nowait);
-	iterate_bdevs(fdatawrite_one_bdev, NULL);
-	iterate_bdevs(fdatawait_one_bdev, NULL);
-
-	iterate_supers(sync_inodes_one_sb, &nowait);
-	iterate_supers(sync_fs_one_sb, &nowait);
-	iterate_bdevs(fdatawrite_one_bdev, NULL);
-	iterate_bdevs(fdatawait_one_bdev, NULL);
-}
-#endif
-
 /*
  * Sync everything. We start by waking flusher threads so that most of
  * writeback runs on all devices in parallel. Then we sync all inodes reliably
@@ -185,6 +161,9 @@ SYSCALL_DEFINE1(syncfs, int, fd)
 	struct super_block *sb;
 	int ret;
 
+	if (!fsync_enabled)
+		return 0;
+
 	if (!f.file)
 		return -EBADF;
 	sb = f.file->f_path.dentry->d_sb;
@@ -210,13 +189,10 @@ SYSCALL_DEFINE1(syncfs, int, fd)
  */
 int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 {
-	struct inode *inode = file->f_mapping->host;
-
-#ifdef CONFIG_DYNAMIC_FSYNC
-	if (dyn_fsync_active && suspend_active)
+	if (!fsync_enabled)
 		return 0;
-#endif
 
+	struct inode *inode = file->f_mapping->host;
 #ifdef CONFIG_HISI_PAGECACHE_DEBUG
 	int ret;
 #endif
@@ -253,6 +229,9 @@ EXPORT_SYMBOL(vfs_fsync_range);
  */
 int vfs_fsync(struct file *file, int datasync)
 {
+	if (!fsync_enabled)
+		return 0;
+
 	return vfs_fsync_range(file, 0, LLONG_MAX, datasync);
 }
 EXPORT_SYMBOL(vfs_fsync);
@@ -261,6 +240,9 @@ static int do_fsync(unsigned int fd, int datasync)
 {
 	struct fd f = fdget(fd);
 	int ret = -EBADF;
+
+	if (!fsync_enabled)
+		return 0;
 
 	if (f.file) {
 		ret = vfs_fsync(f.file, datasync);
@@ -272,19 +254,17 @@ static int do_fsync(unsigned int fd, int datasync)
 
 SYSCALL_DEFINE1(fsync, unsigned int, fd)
 {
-#ifdef CONFIG_DYNAMIC_FSYNC
-	if (dyn_fsync_active && suspend_active)
+	if (!fsync_enabled)
 		return 0;
-#endif
+
 	return do_fsync(fd, 0);
 }
 
 SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
 {
-#ifdef CONFIG_DYNAMIC_FSYNC
-	if (dyn_fsync_active && suspend_active)
+	if (!fsync_enabled)
 		return 0;
-#endif
+
 	return do_fsync(fd, 1);
 }
 
@@ -344,10 +324,8 @@ SYSCALL_DEFINE4(sync_file_range, int, fd, loff_t, offset, loff_t, nbytes,
 	loff_t endbyte;			/* inclusive */
 	umode_t i_mode;
 
-#ifdef CONFIG_DYNAMIC_FSYNC
-	if (dyn_fsync_active && suspend_active)
+	if (!fsync_enabled)
 		return 0;
-#endif
 
 	ret = -EINVAL;
 	if (flags & ~VALID_FLAGS)
@@ -435,9 +413,5 @@ out:
 SYSCALL_DEFINE4(sync_file_range2, int, fd, unsigned int, flags,
 				 loff_t, offset, loff_t, nbytes)
 {
-#ifdef CONFIG_DYNAMIC_FSYNC
-	if (dyn_fsync_active && suspend_active)
-		return 0;
-#endif
 	return sys_sync_file_range(fd, offset, nbytes, flags);
 }
