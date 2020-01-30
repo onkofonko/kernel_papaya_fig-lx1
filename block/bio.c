d83e0ae039b058f6fc5891055e548190706665b6
ockysaurus <ocky852@gmail.com>
2020-01-30 22:56:48 +0100

    do not leak memory in bio_copy_user_iov()

--- block/bio.c
+++ block/bio.c
@@ -1240,8 +1240,11 @@ struct bio *bio_copy_user_iov(struct request_queue *q,
 			}
 		}
 
-		if (((unsigned int)bio_add_pc_page(q, bio, page, bytes, offset)) < bytes)
+		if (((unsigned int)bio_add_pc_page(q, bio, page, bytes, offset)) < bytes) {
+			if (!map_data)
+				__free_page(page);
 			break;
+			}
 
 		len -= bytes;
 		offset = 0;
