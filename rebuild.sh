f82051764ae155a75cbc7f1e8bb550e2857f0eae
Ondrej <ocky852@gmail.com>
2020-03-23 17:42:13 +0100

    add rebuild.sh

---
+++ rebuild.sh
@@ -0,0 +1,14 @@
+#kernel build
+
+#change CROSS_COMPILE location to yours
+
+export CROSS_COMPILE=/home/ondrej/kernel/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-elf/bin/aarch64-elf-
+
+make ARCH=arm64 O=../out papaya_defconfig
+
+make ARCH=arm64 O=../out -j$(nproc --all)
+
+#create kernel.img
+
+/home/ondrej/kernel/github/kernel_papaya/tools/pack_kernerimage_cmd.sh
+
